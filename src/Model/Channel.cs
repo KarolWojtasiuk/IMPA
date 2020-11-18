b6533f3e7eb6dab1bf4befe50ae95947d7b1bba2
KarolWojtasiuk <karolwojtasiuk@gmail.com>
2020-11-18 22:49:42 +0100

    ✨ Add `Channel` and `Message`

---
+++ src/Model/Channel.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+namespace IMPA
+{
+    public class Channel : IIdentifiable, IEquatable<Channel>
+    {
+        internal List<Message> _messages = new();
+
+        public Guid Id { get; init; }
+        public ReadOnlyCollection<Guid> Users { get; init; }
+        public ReadOnlyCollection<Message> Messages
+        {
+            get => _messages.AsReadOnly();
+            init => _messages = value.ToList();
+        }
+        public DateTime CreationDate { get; init; }
+
+        public Channel(Guid firstUser, Guid secondUser)
+        {
+            Id = Guid.NewGuid();
+            Users = new(new List<Guid> { firstUser, secondUser });
+            Messages = new(new List<Message>());
+            CreationDate = DateTime.UtcNow;
+        }
+
+        public bool Equals(Channel? other)
+        {
+            if (other is null)
+            {
+                return false;
+            }
+
+            return Id == other.Id;
+        }
+
+        public override bool Equals(object? obj) => Equals(obj as Channel);
+        public override int GetHashCode() => Id.GetHashCode();
+
+    }
+}
