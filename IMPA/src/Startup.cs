61233e5a507a1a8e9e9a4f69dc2968f35ae5d0ce
KarolWojtasiuk <karolwojtasiuk@gmail.com>
2020-11-20 23:59:48 +0100

    ✨ Add configuration support for database context

--- IMPA/src/Startup.cs
+++ IMPA/src/Startup.cs
@@ -1,12 +1,27 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 
 namespace IMPA
 {
     public class Startup
     {
+        public IConfiguration Configuration;
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public virtual void ConfigureServices(IServiceCollection services)
+        {
+            var dbContext = new MongoContext(Configuration.GetSection("Database").GetValue<string>("ConnectionString"), "IMPA");
+            services.AddSingleton(new DatabaseController(dbContext));
+        }
+
         public static void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
