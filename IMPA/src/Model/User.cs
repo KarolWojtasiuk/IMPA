a95a747f976b5ca7f97cb64f6835931290257c0f
KarolWojtasiuk <karolwojtasiuk@gmail.com>
2020-11-21 01:23:07 +0100

    🛂 Add password hashing

--- IMPA/src/Model/User.cs
+++ IMPA/src/Model/User.cs
@@ -2,6 +2,8 @@ using System;
 using System.Linq;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using MongoDB.Bson.Serialization.Attributes;
+using System.Security.Cryptography;
 
 namespace IMPA
 {
@@ -9,6 +11,10 @@ namespace IMPA
     {
         internal List<Interest> _interests = new();
         internal List<LocationRecord> _locationRecords = new();
+        [BsonElement("PasswordHash")]
+        internal string _passwordHash = String.Empty;
+        [BsonElement("PasswordSalt")]
+        internal string _passwordSalt = String.Empty;
 
         public Guid Id { get; init; }
         public string Username { get; init; }
@@ -29,18 +35,38 @@ namespace IMPA
         public Habits Habits { get; init; }
         public DateTime CreationDate { get; init; }
 
-        public User(string username)
+        public User(string username, string password)
         {
             Id = Guid.NewGuid();
             Username = username;
             FullName = String.Empty;
             Description = String.Empty;
+            ChangePassword(password);
             PersonalityType = PersonalityType.Unkown;
             VisibilityDistance = 50;
             Interests = new(new List<Interest>());
             LocationRecords = new(new List<LocationRecord>());
             Habits = new();
             CreationDate = DateTime.UtcNow;
+        }
+
+        internal void ChangePassword(string password)
+        {
+            using var rfc2898DeriveBytes = new Rfc2898DeriveBytes(password, 16, 100000, HashAlgorithmName.SHA512);
+
+            _passwordHash = Convert.ToBase64String(rfc2898DeriveBytes.GetBytes(16));
+            _passwordSalt = Convert.ToBase64String(rfc2898DeriveBytes.Salt);
+        }
+
+        public bool VerifyPassword(string passwordToVerify)
+        {
+            var salt = Convert.FromBase64String(_passwordSalt);
+            var hash = Convert.FromBase64String(_passwordHash);
+
+            using var rfc2898DeriveBytes = new Rfc2898DeriveBytes(passwordToVerify, salt, 100000, HashAlgorithmName.SHA512);
+
+            var newHash = rfc2898DeriveBytes.GetBytes(16);
+            return newHash.SequenceEqual(hash);
         }
 
         public virtual bool Equals(User? other)
